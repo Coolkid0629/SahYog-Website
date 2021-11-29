92d98ab9fb3eb7d62968610d684204cca29d1c10
Alien178 <57078903+Alien178@users.noreply.github.com>
2021-11-28 19:56:56 -0800

    Add files via upload

--- welcome.js
+++ welcome.js
@@ -1,4 +1,10 @@
-var loader = document.getElementById("preloader");
-window.addEventListener("load", function(){
-    loader.style.display = "none";
-})
+var loaded;
+
+function websiteLoaded() {
+	loaded = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+	document.querySelector(".middle").style.display = "none";
+	document.querySelector(".main").style.display = "block";
+}
